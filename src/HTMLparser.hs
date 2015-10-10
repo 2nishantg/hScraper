bffd2568cc90e7c2fae21e408ae49d5abc6c7509
Ayush Agarwal <agarwal.ayush9@gmail.com>
2015-10-11 01:19:26 +0530

    solved the problem with '\'

---
+++ src/HTMLparser.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction, FlexibleContexts #-}
+module HTMLparser where
+import Control.Monad (liftM, void)
+import Control.Applicative ((<*))
+
+import qualified Data.Text as T
+import Text.Parsec
+import Text.Parsec.Text
+
+
+import Types
+
+parseHtml :: T.Text -> Either ParseError HTMLTree
+parseHtml s = case parse parseNodes "" s of
+                Left err -> Left err
+                Right nodes -> Right $
+                  if length nodes == 1
+                     then head nodes
+                     else Types.toTree "html" [] nodes
+
+parseNodes = spaces >> manyTill parseNode last
+  where
+    last = eof <|> void (try (string "</"))
+
+parseNode = parseElement <|> parseText
+
+parseText = liftM (Types.toLeaf . T.pack) $ many (noneOf "<")
+
+parseElement = do
+  -- opening tag
+  (tag, attrs) <- between (char '<') (char '>') tagData
+  -- contents
+  children <- parseNodes
+  -- closing tag
+  string $ tag ++ ">" -- "</" is consumed by parseNodes, maybe bad form?
+  return $ Types.toTree (T.pack tag) attrs children
+
+tagData = do
+  t <- tagName
+  attrs <- attributes
+  return (t,attrs)
+
+tagName = many1 alphaNum
+
+attributes =  spaces >> many (traillingSpaces attribute)
+
+traillingSpaces a = a <* spaces
+
+attribute = do
+  name <- tagName
+  char '='
+  open <- char '\"' <|> char '\''
+  value <- manyTill anyChar (try $ char open)
+  return (T.pack name, T.pack value)
+
