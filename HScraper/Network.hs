12b260514ec2d48d7db6d93ed459d7fc3f37e417
Nishant9 <nishant.gupta291995@gmail.com>
2015-10-14 06:55:18 +0530

    Support for GET requests.

--- HScraper/Network.hs
+++ HScraper/Network.hs
@@ -4,14 +4,32 @@ import Control.Monad
 
 
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 (pack)
 import qualified Data.ByteString as B
 import Data.Text.Encoding
 import qualified Data.Text as T
 
 import Network.HTTP.Conduit
+import Network.HTTP.Types.Method
 
 -- | A Simple wrapper around simpleHttp so that
 -- it goes along with with other functions.
 fetchResponse :: String -> IO T.Text
 fetchResponse url  =  liftM  (decodeUtf8 . B.concat . BL.toChunks ) $  simpleHttp url
--- Don't know why its necessary that URL ends with a /
+-- Don't know why it is necessary that URL ends with a /
+
+parseParams :: [(String, String)] -> String
+parseParams [] = ""
+parseParams [(name, value)] = name ++ "=" ++ value
+parseParams ((name, value) : xs) = name ++ "=" ++ value ++ "&" ++ parseParams xs
+
+fetchGETRequest :: String -> [(String, String)] -> IO T.Text
+fetchGETRequest url list  = do
+  initReq <- parseUrl url
+  let req = initReq
+            { method = methodGet
+            , queryString = Data.ByteString.Char8.pack $ parseParams list
+            }
+  manager <- newManager tlsManagerSettings
+  res <- httpLbs req manager
+  return $  decodeUtf8 . B.concat . BL.toChunks $ responseBody res
