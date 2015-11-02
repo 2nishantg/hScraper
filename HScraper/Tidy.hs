4a5682108df56aeccfe93d3d72f18239c20d1de8
Nishant9 <nishant.gupta291995@gmail.com>
2015-11-02 16:53:14 +0530

    make temporary files in current directory.

--- HScraper/Tidy.hs
+++ HScraper/Tidy.hs
@@ -4,9 +4,12 @@ module HScraper.Tidy (
 import Data.Text as T
 import Data.Text.IO as TIO
 import System.Process
+import System.Directory(getCurrentDirectory)
 
 tidy ::T.Text -> IO T.Text
 tidy t  = do
-  TIO.writeFile "/home/nis/hscraper_temp.html" t
-  (_,Just hout,_,_) <- createProcess (proc "tidy" ["-q","-f", "/home/nis/hscraper_webpages.logs", "/home/nis/hscraper_temp.html"]){ std_out = CreatePipe }
+  pwd <- System.Directory.getCurrentDirectory
+  let tempFile = pwd ++ "/hscraper_temp.html"
+  TIO.writeFile tempFile  t
+  (_,Just hout,_,_) <- createProcess (proc "tidy" ["-q","-f", "/home/nis/hscraper_webpages.logs", tempFile]){ std_out = CreatePipe }
   TIO.hGetContents hout
