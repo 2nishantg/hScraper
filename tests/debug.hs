75fae8fafc993ee46853bfa59e7a5e1946d50abe
Nishant9 <nishant.gupta291995@gmail.com>
2015-11-06 04:11:37 +0530

    Add some more debugging helpers.

--- tests/debug.hs
+++ tests/debug.hs
@@ -5,11 +5,15 @@ import HScraper.Query
 import HScraper.Tidy
 import HScraper.Types
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 main = putStrLn "Hi"
 
-getStringFromFile :: String -> IO String
-getStringFromFile str = unwords . words  <$> ( openFile str ReadMode >>= hGetContents)
+getSourceFromFile :: String -> IO String
+getSourceFromFile str = unwords . words  <$> ( openFile str ReadMode >>= hGetContents)
+
+getFromFile :: String -> IO HTMLTree
+getFromFile str = getParsedHTML <$>  parseHtml <$> (openFile str ReadMode >>= TIO.hGetContents)
 
 applyOnIO :: (a -> b) -> IO a -> IO b
 applyOnIO f a = f <$> a
@@ -21,4 +25,5 @@ getParsedHTML :: Either a HTMLTree -> HTMLTree
 getParsedHTML = either (const NullTree) id
 
 getParsedQuery :: Either a Query -> Query
-getParsedQuery = either (const []) id 
+getParsedQuery = either (const []) id
+
