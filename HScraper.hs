d7f164a0d0f26f0939330096ebc193d649d3c1db
Nishant9 <nishant.gupta291995@gmail.com>
2015-11-06 16:48:19 +0530

    Restructure the library.

---
+++ HScraper.hs
@@ -0,0 +1,17 @@
+module HScraper (
+  module HScraper.HTMLparser ,
+  module HScraper.Network ,
+  module HScraper.Query ,
+  module HScraper.Types ,
+  module HScraper.Tidy ,
+  module HScraper.Main
+  ) where
+
+
+import HScraper.HTMLparser
+import HScraper.Network
+import HScraper.Query
+import HScraper.Tidy
+import HScraper.Types
+import HScraper.Main
+
